194c231a051927dc26b40f10f57a80307b6e2902
Atlas <yangyuxin@atlasyang.local>
2015-09-27 16:49:53 +0800

    test Upadate

--- LightUp/NetworkTestViewController.m
+++ LightUp/NetworkTestViewController.m
@@ -18,4 +18,46 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+- (IBAction)login:(id)sender {
+}
+
+- (IBAction)reg:(id)sender {
+}
+
+- (IBAction)send:(id)sender {
+}
+
+- (IBAction)info:(id)sender {
+}
+
+- (IBAction)remark:(id)sender {
+}
+
+- (IBAction)addremark:(id)sender {
+}
+
+- (IBAction)location:(id)sender {
+}
+
+- (IBAction)detailMessage:(id)sender {
+}
+
+- (IBAction)districk:(id)sender {
+}
+
+- (IBAction)page:(id)sender {
+}
+
+- (IBAction)follow:(id)sender {
+}
+
+- (IBAction)search:(id)sender {
+}
+
+- (IBAction)edit:(id)sender {
+}
+
+- (IBAction)headshot:(id)sender {
+}
 @end
